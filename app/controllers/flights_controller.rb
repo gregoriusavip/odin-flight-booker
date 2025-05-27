752dd32152d5b8a8bcfcdeb20cb91d3268ba1c0c
Gregorius Avip <71865513+gregoriusavip@users.noreply.github.com>
2025-05-26 20:50:24 -0700

    Create flights query

---
+++ app/controllers/flights_controller.rb
@@ -0,0 +1,14 @@
+class FlightsController < ApplicationController
+  def index
+    if params[:flight].present?
+      p = allowed_flights_params
+      @flights = Flight.where(departure_airport_id: p[:departure_airport_id], arrival_airport_id: p[:arrival_airport_id], departure: p[:departure]..)
+    end
+  end
+
+  private
+
+  def allowed_flights_params
+    params.expect(flight: [ :departure_airport_id, :arrival_airport_id, :passengers, :departure ])
+  end
+end
